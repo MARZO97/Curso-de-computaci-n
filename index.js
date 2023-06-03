dd471d763d2fda34dd9038f9a56e66f9f865758b
MARZO97 <135179923+MARZO97@users.noreply.github.com>
2023-06-03 15:21:47 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const miBoton = document.getElementById('miBoton')
+function handleClick () {
+    alert('¡Hola! has hecho clic en el boton');
+}
+miboton.addEventListener("click", function() {
+    alert("¡Haz hecho clic en el boton!");
+});
